54e3b041e84d239b54365890496f12889d37e9bb
Ching-Yee-Chan <fkzxy4213@qq.com>
2022-10-27 15:39:43 +0800

    initial 5 stage pipeline

---
+++ pipeline.ip_user_files/sim_scripts/data_ram/ies/run.f
@@ -0,0 +1,16 @@
+-makelib ies/xil_defaultlib -sv \
+  "E:/vivado/Vivado/2017.1/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib ies/xpm \
+  "E:/vivado/Vivado/2017.1/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib ies/blk_mem_gen_v8_3_6 \
+  "../../../ipstatic/simulation/blk_mem_gen_v8_3.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  "../../../../pipeline.srcs/sources_1/ip/data_ram/sim/data_ram.v" \
+-endlib
+-makelib ies/xil_defaultlib \
+  glbl.v
+-endlib
+
